ef8abcab89598c633832e3c48bdc89960455ee65
Allan <allanmendoza036@gmail.com>
2025-09-14 00:02:56 -0600

    update

--- proyecto/.next/static/chunks/Documents_GitHub_Proyecto_Desarrollo_Web2_proyecto_src_app_layout_tsx_50aef86c._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__7587f474._.css",
-  "static/chunks/Documents_GitHub_Proyecto_Desarrollo_Web2_proyecto_b9b5e594._.js"
-],
-    source: "dynamic"
-});
